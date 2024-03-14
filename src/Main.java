5b4a2983a62e806235df608279b16207ac3749c9
Namnnnnnne <145822566+Namnnnnnne@users.noreply.github.com>
2024-03-14 07:57:01 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        MP3 mp3 = new MP3("See you again", "Nhac hay", "MP3-123", 200, 180);
 
-        MP3 mp3;
+        Book book = new Book("Hoc them tieng anh", "Sach hay", "123K", 100, "Namne", 300, "dayhoc");
+
+        System.out.println("MP3 Information:");
         mp3.showInfo();
+        System.out.println();
 
-        Book book;
+        System.out.println("Book Information:");
         book.showInfo();
-
     }
 }
